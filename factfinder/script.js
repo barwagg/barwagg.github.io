23f529bfe9f504286a47d7fa9ad8294f4dd67f48
barwagg <77997742+barwagg@users.noreply.github.com>
2021-01-25 14:42:38 -0800

    uploaded factfinder for the first time

---
+++ factfinder/script.js
@@ -0,0 +1,36 @@
+
+document.querySelector("#fact1").addEventListener("click",displayFact1);
+
+function displayFact1(){
+	document.querySelector("#dispfact").innerHTML = "Squirrels are behind most power outages in the U.S.";
+	document.querySelector("#dispimg").innerHTML = "<img src = 'images/squirrel.jpg'>";
+}
+
+document.querySelector("#fact2").addEventListener("click",displayFact2);
+
+function displayFact2(){
+	document.querySelector("#dispfact").innerHTML = "You're more likely to get a computer virus from visiting religious sites than porn sites.";
+	document.querySelector("#dispimg").innerHTML = "<img src = 'images/virus.jpg'>";
+}
+
+document.querySelector("#fact3").addEventListener("click",displayFact3);
+
+function displayFact3(){
+	document.querySelector("#dispfact").innerHTML = "The inventor of the Pringles can is now buried in one.";
+	document.querySelector("#dispimg").innerHTML = "<img src = 'images/pringles.jpg'>";
+}
+
+document.querySelector("#fact4").addEventListener("click",displayFact4);
+
+function displayFact4(){
+	document.querySelector("#dispfact").innerHTML = "Sunglasses were originally designed for Chinese judges to hide their facial expressions in court.";
+	document.querySelector("#dispimg").innerHTML = "<img src = 'images/glasses.jpg'>";
+}
+
+document.querySelector("#fact5").addEventListener("click",displayFact5);
+
+function displayFact5(){
+	document.querySelector("#dispfact").innerHTML = "Cotton candy was invented by a dentist.";
+	document.querySelector("#dispimg").innerHTML = "<img src = 'images/cottoncandy.jpg'>";
+}
+
